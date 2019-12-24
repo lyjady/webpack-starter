51c7792b25bb30dd679e19b7aa2ef6764a209ae5
lyjady <lyj2780866735>
2019-12-24 22:01:19 +0800

    更新代码

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@
 //浏览器无法解析ES6的语法,这一行会报错
 import $ from 'jquery'
 
+//导入css样式表
+//webpack只能处理JavaScript文件,无法处理其他的非JavaScript文件,需要手动安装第三方的加载器
+//style-loader css-loader
+import './css/index.css'
+
 $(function () {
     $('li:odd').css('backgroundColor', 'skyblue');
     $('li:even').css('backgroundColor', function () {
@@ -13,3 +18,11 @@ $(function () {
 //1.webpack能够处理JS文件的互相依赖关系
 //2.webpack能够处理JS的兼容问题,把高级的浏览器不识别的语法转成低级的识别的语法
 //运行指令 webpack srcDir -o destDir mode=development
+//-----------------------------------------------------------------------
+//1.webpack-dev-server工具能够实现"热部署",只要修改了main.js文件会就自动编译生成新的bundle.js文件在内存中而并不是将本地文件给覆盖
+//将生成的bundle.js文件在服务器根目录给暴露出来。如果想让页面引用bundle.js文件就要修改index.html中的js文件的地址
+//2.在webpack3的版本中不能直接使用webpack-dev-server启动服务需要在webpack.config.js文件中配置才能启动
+// "scripts": {
+//   "dev": "webpack-dev-server"
+// }
+//在webpack4中能够直接启动
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const webpack = require('webpack'); //启动热更新第二步
+//导入在内存中生成html页面的插件
+const htmlWebpackPlugin = require('html-webpack-plugin');
 //这个配置文件起始就是一个js文件,通过Node中的模块操作,向外暴露了一个配置对象
 module.exports = {
     //srcDir表示要打包的那个问价
@@ -9,6 +12,28 @@ module.exports = {
         path: path.join(__dirname, './dist'),
         //指定生成的文件名称
         filename: "bundle.js"
+    },
+    //第二种webpack-dev-server的配置参数
+    devServer: {
+        open: true,
+        port: 3000,
+        contentBase: 'src',
+        hot: true //启动热更新第一步
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(), //new一个热更新的模块对象,热更新的第三步
+        //创建一个在内存中生成html页面的插件
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './src/index.html'), //指定模板页面,根据这个页面生成内存中的页面
+            filename: "index.html" //指定生成页面的名称
+        })
+    ],
+    //这个节点用于配置所有的第三方加载器
+    module: {
+        rules: [
+            //将css后缀的文件交给style-loader和css-loader进行处理
+            {test: /\.css$/, use: ['style-loader', 'css-loader']}
+        ]
     }
 };
 //以后要重新打包只要直接输入指令webpack,会自动的读取配置文件中的配置进行打包
